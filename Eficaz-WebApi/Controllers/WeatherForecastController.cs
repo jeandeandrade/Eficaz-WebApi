fd911ab42f056324f0540a6fc1ea5c57bbc90486
Jean Andrade <jeancarlos.av0@gmail.com>
2024-10-24 11:02:49 -0300

    Teste do pre-commit hook para formatação

--- Eficaz-WebApi/Controllers/WeatherForecastController.cs
+++ Eficaz-WebApi/Controllers/WeatherForecastController.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
-
 namespace Eficaz_WebApi.Controllers
 {
     [ApiController]
@@ -7,27 +6,15 @@ namespace Eficaz_WebApi.Controllers
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
-
+     { "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    }; private readonly ILogger<WeatherForecastController> _logger; public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        { _logger = logger; }
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            { Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)), TemperatureC = Random.Shared.Next(-20, 55), Summary = Summaries[Random.Shared.Next(Summaries.Length)] })
+.ToArray();
         }
     }
 }
