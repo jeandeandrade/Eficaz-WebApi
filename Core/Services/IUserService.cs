dc404385584fc36aff93c41205ade84896757502
Jean Andrade <jeancarlos.av0@gmail.com>
2024-10-27 20:21:38 -0300

    [Core/Services] - Commit Feature | Modificado as interfaces do Core.Repositories e criado as interfaces do Core.Services

--- Core/Services/IUserService.cs
+++ Core/Services/IUserService.cs
@@ -1,8 +1,8 @@
 using Core.Models;
 
-namespace Core.Repositories
+namespace Core.Services
 {
-    public interface IUserRepository
+    public interface IUserService
     {
         public Task<User> GetUser(string userId);
         public Task<User> AddUser(string userId, User user);
